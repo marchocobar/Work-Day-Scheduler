2df423cbe8fbdca04f5b042ad24bd51cff3ceec7
Marissa Baron <mariluvv23@gmail.com>
2022-10-07 15:20:41 -0400

    events

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").text(currentDate);
 
+var currentTime = moment().format("HH");
 var timeBlock = $(".time-block");
 var saveBtn = $('.saveBtn');
 var descriptionInput = $('.description')
+var hourBlocks = [
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18"
+]
+
+var eventValues = []
 
 function inputUserSchedule() {
-descriptionInput = $('<textarea>').value();
-timeBlock.append('<p>' + descriptionInput + '<p>');
+    var pullEvents = [];
+    for (var i = 0; i < hourBlocks.length; i++) {
+        pullEvents.push(descriptionInput[i].value)
+    }
+    eventValues = pullEvents;
+    localStorage.setItem('event', JSON.stringify(eventValues));
+    alert("Saved!");
+
 }
 
-saveBtn.click(inputUserSchedule());
+function showEvents() {
+    if (JSON.parse(localStorage.getItem('event'))) {
+        eventValues =  (JSON.parse(localStorage.getItem('event')));
+    }
+    for (var i = 0; i < hourBlocks.length; i++) {
+        if (eventValues[i]) {
+            descriptionInput[i].value(eventValues[i])
+        }
+    }
+}
+
+function colorBlocks() {
+    for (var i = 0; i < hourBlocks.length; i++) {
+        if (currentTime<hourBlocks[i]) {
+            descriptionInput[i].addClass("future");
+        }
+    }
+}
+
+saveBtn.click(inputUserSchedule);
+showEvents();
+colorBlocks();
+
