4cdaf5c22a86c543077eda2850674eda21b89b7a
Marissa Baron <mariluvv23@gmail.com>
2022-10-07 12:22:03 -0400

    some code

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").text(currentDate);
+
+var timeBlock = $(".time-block");
+var saveBtn = $('.saveBtn');
+var descriptionInput = $('.description')
+
+function inputUserSchedule() {
+descriptionInput = $('<textarea>').value();
+timeBlock.append('<p>' + descriptionInput + '<p>');
+}
+
+saveBtn.click(inputUserSchedule());
